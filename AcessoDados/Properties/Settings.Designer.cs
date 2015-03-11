71b87a74c8181829ea61e117801d72317f29b441
albertosveloso <albertosveloso@gmail.com>
2015-03-10 23:09:03 -0300

    Commit Inicial para retorno desenvolvimento

--- AcessoDados/Properties/Settings.Designer.cs
+++ AcessoDados/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace AcessoDados.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,8 +26,8 @@ namespace AcessoDados.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=NOTEBOOKBETO\\SQLEXPRESS;Initial Catalog=OrdemServico;Integrated Secur" +
-            "ity=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=PC-ALBERTO\\SQLEXPRESS;Initial Catalog=OrdemServico;Integrated Securit" +
+            "y=True")]
         public string StringConexao {
             get {
                 return ((string)(this["StringConexao"]));
